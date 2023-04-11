41b05bb444734fc0ddd95a0461256d4317e6155b
Daxsum <kaldax250@gmail.com>
2023-04-11 20:40:20 +0300

    feat:adding task and project ui and implmentation complited

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -1,4 +1,4 @@
-// import { delay } from "@/lib/async";
+import { delay } from "@/lib/async";
 import Greeting from "@/components/Greeting";
 import GreetingsSkeleton from "@/components/GreetingsSkeleton";
 import { getUserFromCookie } from "@/lib/auth";
@@ -6,8 +6,24 @@ import { db } from "@/lib/db";
 import { cookies } from "next/headers";
 import Link from "next/link";
 import { Suspense } from "react";
+import ProjectCard from "@/components/ProjectCard";
+import TaskCard from "../../../components/TaskCard";
+const getData = async () => {
+  await delay(2000);
+  const user = await getUserFromCookie(cookies());
+  const projects = await db.project.findMany({
+    where: {
+      ownerId: user?.id,
+    },
+    include: {
+      tasks: true,
+    },
+  });
 
+  return { projects };
+};
 export default async function Page() {
+  const { projects } = await getData();
   return (
     <div className="h-full overflow-y-auto pr-6 w-full">
       <div className=" h-full items-stretch justify-center min-h-[content]">
@@ -17,11 +33,20 @@ export default async function Page() {
           </Suspense>
         </div>
         <div className="flex flex-2 grow items-center flex-wrap mt-3 -m-3 ">
-          {/** projects map here */}
+          {projects.map((project) => (
+            <div className="w-1/3 p-3" key={project.id}>
+              <Link href={`/project/${project.id}`}>
+                <ProjectCard project={project} />
+              </Link>
+            </div>
+          ))}
+
           <div className="w-1/3 p-3">{/* new project here */}</div>
         </div>
         <div className="mt-6 flex-2 grow w-full flex">
-          <div className="w-full">{/* tasks here */}</div>
+          <div className="w-full">
+            <TaskCard />
+          </div>
         </div>
       </div>
     </div>
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,3 +1,4 @@
+import { hashPassword } from "@/lib/auth";
 import { db } from "@/lib/db";
 import { TASK_STATUS } from "@prisma/client";
 
@@ -11,7 +12,6 @@ const getRandomTaskStatus = () => {
 };
 
 async function main() {
-  
   const user = await db.user.upsert({
     where: { email: "user@email.com" },
     update: {},
@@ -19,7 +19,7 @@ async function main() {
       email: "user@email.com",
       firstName: "User",
       lastName: "Person",
-      password: "password",
+      password: await hashPassword("password"),
       projects: {
         create: new Array(5).fill(1).map((_, i) => ({
           name: `Project ${i}`,
